1ee4580c46ce5ee6ac8d1b8288ea21a69cb8e7bd
luoxianjun <luoxianjun@cumt.edu.cn>
2024-06-13 16:26:21 +0800

    API调用

--- hm-api/src/main/java/com/hmall/api/clients/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/clients/ItemClient.java
@@ -2,8 +2,10 @@ package com.hmall.api.clients;
 
 
 import com.hmall.api.domain.dto.ItemDTO;
+import com.hmall.api.domain.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
@@ -13,4 +15,7 @@ import java.util.List;
 public interface ItemClient {
     @GetMapping("/items")
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+    @GetMapping("/items/stock/deduct")
+    void deductStock(@RequestBody List<OrderDetailDTO> items);
 }
